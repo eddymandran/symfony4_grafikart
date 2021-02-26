b42b65cf3944cbdefa291c82826e74e1fcee6c8c
eddymandran <eddy.mandran@gmail.com>
2021-02-26 12:08:17 +0100

    adding first property

--- MaSuperAgence/src/Controller/PropertyController.php
+++ MaSuperAgence/src/Controller/PropertyController.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Controller;
 
+use App\Entity\Property;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -14,6 +15,24 @@ class PropertyController extends AbstractController{
 
     public function index(): Response
     {
+        $property = new Property();
+
+       $property->setTitle('Mon premier bien')
+        -> setPrice(200000)
+        -> setRooms(4)
+        -> setBedrooms(3)
+        -> setDescription('Une petite description')
+        -> setSurface('60')
+        -> setFloor(4)
+        -> setHeat(1)
+        -> setCity('Montpellier')
+        -> setAddress('15 boulevard Gambetta')
+        -> setPostalCode('34000'); 
+
+        $em = $this->getDoctrine()-> getManager();
+        $em -> persist($property);
+        $em -> flush();
+
         return $this->render('property/index.html.twig', ['current_menu'=> 'properties']);
     }
 }
